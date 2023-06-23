334a99d3beabf71e9199080f3662bad23a2ed6a8
msi1995 <lloyddo@oregonstate.edu>
2023-06-23 06:33:44 -0700

    add initial spotlight styling to prevent fail to dark until mouse event

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -2,9 +2,14 @@ import React, { useEffect } from "react";
 
 export const Spotlight: React.FC = () => {
   useEffect(() => {
+
+    //required to prevent darkening not applying until initial mouse movement
+    const spotlight = document.querySelector("#spotlight") as HTMLElement;
+    spotlight.style.background = `radial-gradient(circle at 0px 0px, #00000000 20px, #00000050 350px)`;
+
     const handleMouseMove = (event: MouseEvent) => {
       const spotlight = document.querySelector("#spotlight") as HTMLElement;
-      const { clientX, clientY } = event;
+      const { clientX = 0, clientY = 0  } = event;
 
       setTimeout(() => {
         spotlight.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 20px, #00000050 350px)`;

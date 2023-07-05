e2baf66525de189aeb0a46cd03f31dfaeda050d3
msi1995 <lloyddo@oregonstate.edu>
2023-07-05 15:23:19 -0700

    resume update and catch audio crash

--- src/components/EntryPage.tsx
+++ src/components/EntryPage.tsx
@@ -8,7 +8,13 @@ export const EntryPage = ({ setEntryPageActive }: EntryPageProps) => {
   const entryAudio = new Audio("/doorunlocking.mp3");
 
   const doEntryThings = async () => {
-    await entryAudio.play();
+    try{
+      await entryAudio.play();
+    }
+    catch(e){
+      //Some versions of Chrome v50+ have rare bug where audio can cause page crash. 
+      console.log(`error -- ${e}`)
+    }
     await new Promise((resolve) => setTimeout(resolve, 50));
     setEntryPageActive(false);
   };
